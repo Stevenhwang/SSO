88bfca8394fb1cf1da336d25d9961925141215e4
zed <zed@itcom888.com>
2020-01-06 19:15:42 +0800

    start.py

--- start.py
+++ start.py
@@ -6,7 +6,8 @@ from routers import routers
 
 logging.basicConfig(level=logging.DEBUG)
 app = Sanic(name='admin')
-app.config.update(server_settings, db_settings)
+app.config.update(server_settings)
+app.config.update(db_settings)
 
 
 @app.middleware('request')
@@ -22,10 +23,30 @@ async def parse_query(request):
 for router in routers:
     app.add_route(*router, version=2)
 
+db_config = {
+    'connections': {
+        'default': {
+            'engine': 'tortoise.backends.mysql',
+            'credentials': {
+                'host': app.config.DB_HOST,
+                'port': app.config.DB_PORT,
+                'user': app.config.DB_USER,
+                'password': app.config.DB_PASS,
+                'database': app.config.DB_NAME
+            }
+        }
+    },
+    'apps': {
+        'models': {
+            'models': ["models.admin"],
+            'default_connection': 'default',
+        }
+    }
+}
+
 register_tortoise(
     app,
-    db_url=f"mysql://{app.config.DB_USER}:{app.config.DB_PASS}@{app.config.DB_HOST}:{app.config.DB_PORT}/{app.config.DB_NAME}",
-    modules={"models": ["models.admin"]},
+    config=db_config,
     generate_schemas=True
 )
 
