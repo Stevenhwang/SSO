108280cf99fa9402f5cb2af51bedb120220bc937
zed <zed@itcom888.com>
2020-01-07 18:06:01 +0800

    components view

--- routers.py
+++ routers.py
@@ -1,6 +1,7 @@
 from views.users_view import UsersView, UserView
 from views.roles_view import RolesView, RoleView
 from views.menus_view import MenusView, MenuView
+from views.components_view import ComponentsView, ComponentView
 
 routers = [
     (UsersView.as_view(), '/users'),
@@ -9,4 +10,6 @@ routers = [
     (RoleView.as_view(), '/role/<rid:int>'),
     (MenusView.as_view(), '/menus'),
     (MenuView.as_view(), '/menu/<mid:int>'),
+    (ComponentsView.as_view(), '/components'),
+    (ComponentView.as_view(), '/component/<cid:int>'),
 ]
