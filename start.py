e2980a61ddefa01e3ecf450153bf721c36fd42b4
zed <zed@itcom888.com>
2020-01-07 19:35:09 +0800

    common blueprint

--- start.py
+++ start.py
@@ -1,39 +1,15 @@
 from sanic import Sanic
-from sanic.response import json
 import logging
 from tortoise.contrib.sanic import register_tortoise
 from settings import server_settings, db_settings
-from routers import routers
-from utils.jwt_token import AuthToken
-from utils.redis_conn import redis_conn
+from blueprints.common import common_bp
 
 logging.basicConfig(level=logging.DEBUG)
 app = Sanic(name='admin')
 app.config.update(server_settings)
 app.config.update(db_settings)
 
-
-@app.middleware('request')
-async def use_auth(request):
-    # auth middleware
-    token = request.headers.get('Auth-Token')
-    user_info = AuthToken().decode_token(token)
-    if not token:
-        return json(dict(code=-1, msg="请登录！"), status=401)
-    if not user_info:
-        return json(dict(code=-1, msg="Token失效!"), status=401)
-    temp_token = await redis_conn('get', f"uid_{user_info.get('user_id')}_auth_token")
-    if not temp_token or temp_token.decode("utf-8") != token:
-        return json(dict(code=-1, msg="Token失效!"), status=401)
-    request.ctx.user_id = user_info.get('user_id')
-    request.ctx.username = user_info.get('username')
-    request.ctx.is_super = user_info.get('is_super')
-    # parse query args middleware
-    request.ctx.query = eval(str(request.args).replace('[', '').replace(']', ''))
-
-
-for router in routers:
-    app.add_route(*router, version=2)
+app.blueprint(common_bp)
 
 db_config = {
     'connections': {
