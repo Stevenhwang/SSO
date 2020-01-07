bbbfd78c9db80838c1d6afeea74ae1e2de88ddce
zed <zed@itcom888.com>
2020-01-07 17:58:22 +0800

    menus view

--- routers.py
+++ routers.py
@@ -1,9 +1,12 @@
 from views.users_view import UsersView, UserView
 from views.roles_view import RolesView, RoleView
+from views.menus_view import MenusView, MenuView
 
 routers = [
     (UsersView.as_view(), '/users'),
     (UserView.as_view(), '/user/<uid:int>'),
     (RolesView.as_view(), '/roles'),
-    (RoleView.as_view(), '/role/<rid:int>')
+    (RoleView.as_view(), '/role/<rid:int>'),
+    (MenusView.as_view(), '/menus'),
+    (MenuView.as_view(), '/menu/<mid:int>'),
 ]
