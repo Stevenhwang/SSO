00b79302e0c6b05f9068bb2a972087d061a0e0c6
zed <zed@itcom888.com>
2020-01-07 17:33:04 +0800

    roles_view

--- routers.py
+++ routers.py
@@ -1,6 +1,9 @@
 from views.users_view import UsersView, UserView
+from views.roles_view import RolesView, RoleView
 
 routers = [
     (UsersView.as_view(), '/users'),
-    (UserView.as_view(), '/user/<uid:int>')
+    (UserView.as_view(), '/user/<uid:int>'),
+    (RolesView.as_view(), '/roles'),
+    (RoleView.as_view(), '/role/<rid:int>')
 ]
---
+++ views/roles_view.py
@@ -0,0 +1,55 @@
+from sanic.response import json
+from sanic.views import HTTPMethodView
+from models.admin import Role
+
+
+class RolesView(HTTPMethodView):
+    async def get(self, request):
+        roles = await Role.search(**request.ctx.query)
+        return json(dict(data=[role.to_dict(exclude=['users', 'components', 'menus', 'functions']) for role in roles],
+                         count=len(roles), code=0, msg="成功"))
+
+    async def post(self, request):
+        data = request.json
+        if not data.get('name'):
+            return json(dict(code=-1, msg='关键参数不能为空'))
+        er = await Role.get_or_none(name=data.get('name'))
+        if er:
+            return json(dict(code=-1, msg='此角色已注册！'))
+        nr = Role(**data)
+        await nr.save()
+        return json(dict(code=0, msg='角色创建成功!'))
+
+
+class RoleView(HTTPMethodView):
+    async def put(self, request, rid):
+        data = request.json
+        r = await Role.get_or_none(id=rid)
+        if not r:
+            return json(dict(code=-1, msg='角色不存在'))
+        for k, v in data.items():
+            setattr(r, k, v)
+        await r.save()
+        return json(dict(code=0, msg='编辑成功'))
+
+    async def delete(self, request, rid):
+        r = await Role.get_or_none(id=rid)
+        if not r:
+            return json(dict(code=-1, msg='角色不存在'))
+        await r.delete()
+        return json(dict(code=0, msg='删除成功'))
+
+    async def patch(self, request, rid):
+        # 角色启用禁用
+        r = await Role.get_or_none(id=rid)
+        if not r:
+            return json(dict(code=-1, msg='角色不存在'))
+        status = r.status
+        if status:
+            r.status = False
+            await r.save()
+            return json(dict(code=0, msg='角色禁用成功'))
+        else:
+            r.status = True
+            await r.save()
+            return json(dict(code=0, msg='角色启用成功'))
