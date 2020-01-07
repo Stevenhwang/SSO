ed0f100e5bb64e215cec89d6d970aba99c6b2af1
zed <zed@itcom888.com>
2020-01-07 18:44:43 +0800

    logout view

--- views/users_view.py
+++ views/users_view.py
@@ -66,3 +66,33 @@ class UserView(HTTPMethodView):
             u.status = True
             await u.save()
             return json(dict(code=0, msg='用户启用成功'))
+
+
+class LogoutView(HTTPMethodView):
+    async def post(self, request):
+        # 登出
+        uid = request.ctx.user_id
+        await redis_conn('delete', f"uid_{uid}_auth_token")
+        return json(dict(code=0, msg='注销成功'))
+
+    async def patch(self, request):
+        # 用户重置密码
+        data = request.json
+        old_password = data.get('old_password')
+        new_password1 = data.get('new_password1')
+        new_password2 = data.get('new_password2')
+        uid = request.ctx.user_id
+
+        if not old_password or not new_password1 or not new_password2:
+            return json(dict(code=-1, msg='不能有空值'))
+
+        if new_password1 != new_password2:
+            return json(dict(code=-2, msg='两个新密码输入不一致'))
+
+        user = await User.get(id=uid)
+        if user.password != gen_md5(old_password):
+            return json(dict(code=-3, msg='密码错误'))
+        user.password = gen_md5(new_password1)
+        await user.save()
+
+        return json(dict(code=0, msg='修改密码成功'))
