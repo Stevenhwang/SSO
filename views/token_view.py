ddb5f0b354e6ed8206b6785a68a7ca7e2a52eb1f
zed <zed@itcom888.com>
2020-01-08 19:22:09 +0800

    token view

--- views/token_view.py
+++ views/token_view.py
@@ -4,15 +4,17 @@ from utils.redis_conn import redis_conn
 
 
 class TokenView(HTTPMethodView):
-    def post(self, request):
+    async def post(self, request):
         # 清除token
         data = request.json
         users = data.get("users")
+        keys = []
         for uid in users:
-            await redis_conn('delete', f"uid_{uid}_auth_token")
+            keys.append(f"uid_{uid}_auth_token")
+        await redis_conn('del', *keys)
         return json(dict(code=0, msg='清除token成功!'))
 
-    def put(self, request):
+    async def put(self, request):
         # 长期token
         data = request.json
         users = data.get("users")
