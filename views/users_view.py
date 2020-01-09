e53b8d78cdc099810e891df6482c7bdd12fbc25a
zed <zed@itcom888.com>
2020-01-09 14:19:12 +0800

    fix

--- views/users_view.py
+++ views/users_view.py
@@ -71,7 +71,7 @@ class LogoutView(HTTPMethodView):
     async def post(self, request):
         # 登出
         uid = request.ctx.user_id
-        await redis_conn('delete', f"uid_{uid}_auth_token")
+        await request.app.redis.execute('delete', f"uid_{uid}_auth_token")
         return json(dict(code=0, msg='注销成功'))
 
     async def put(self, request):
