e23bddccf7b2c4495fc443ddab33eb8af28e3a41
zed <zed@itcom888.com>
2020-01-09 12:25:25 +0800

    redis connection

--- utils/redis_conn.py
+++ utils/redis_conn.py
@@ -3,7 +3,7 @@ from settings import redis_settings
 
 
 async def redis_conn(*args):
-    redis = await aioredis.create_redis_pool(
+    redis = await aioredis.create_connection(
         f"redis://{redis_settings.get('REDIS_HOST')}:{redis_settings.get('REDIS_PORT')}",
         password=redis_settings.get('REDIS_PASS'),
         db=redis_settings.get('REDIS_DB')
