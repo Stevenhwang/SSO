e96a69d0e773282d19007a5d0496a95f1a27b97f
zed <zed@itcom888.com>
2020-01-10 16:02:41 +0800

    upload log api

--- blueprints/gateway.py
+++ blueprints/gateway.py
@@ -1,6 +1,6 @@
 from sanic.response import text
 from sanic import Blueprint
-from models.admin import User, Role
+from models.admin import User, Role, SysLog
 import re
 
 gateway_bp = Blueprint('gateway', version=2)
@@ -8,6 +8,7 @@ gateway_bp = Blueprint('gateway', version=2)
 
 @gateway_bp.route('/auth_check')
 async def auth_check(request):
+    # 网关查询路由权限
     args = eval(str(request.args).replace('[', '').replace(']', ''))
     uid = args.get('uid')
     uri = args.get('uri')
@@ -22,3 +23,12 @@ async def auth_check(request):
                 if f.method_type == method and re.match(r'{}'.format(f.uri), uri) and f.status:
                     return text("true")
         return text("false")
+
+
+@gateway_bp.route('/log')
+async def upload_log(request):
+    # 网关上传日志接口
+    data = request.json
+    new_log = SysLog(**data)
+    await new_log.save()
+    return text('success')
