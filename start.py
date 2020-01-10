5db933413fe245e3a7ba3c17ab244f535bd3f3ef
zed <zed@itcom888.com>
2020-01-10 16:59:10 +0800

    settings

--- start.py
+++ start.py
@@ -3,41 +3,15 @@ from sanic.response import text
 from sanic.exceptions import NotFound
 import logging
 from tortoise.contrib.sanic import register_tortoise
-from settings import server_settings, db_settings
+from settings import server_settings, db_config
 from blueprints import api
 import aioredis
 from settings import redis_settings
-# from tasks.log_sub import log_sub
 
 logging.basicConfig(level=logging.DEBUG)
 app = Sanic(name='admin')
-app.config.update(server_settings)
-app.config.update(db_settings)
-
 app.blueprint(api)
 
-db_config = {
-    'connections': {
-        'default': {
-            'engine': 'tortoise.backends.mysql',
-            'credentials': {
-                'host': app.config.DB_HOST,
-                'port': app.config.DB_PORT,
-                'user': app.config.DB_USER,
-                'password': app.config.DB_PASS,
-                'database': app.config.DB_NAME
-            }
-        }
-    },
-    'apps': {
-        'models': {
-            'models': ["models.admin"],
-            'default_connection': 'default',
-        }
-    }
-}
-
-# 初始化数据库连接
 register_tortoise(
     app,
     config=db_config,
@@ -45,7 +19,6 @@ register_tortoise(
 )
 
 
-# 初始化redis连接池
 @app.listener('before_server_start')
 async def init_redis_pool(app, loop):
     redis = await aioredis.create_redis_pool(
@@ -56,28 +29,15 @@ async def init_redis_pool(app, loop):
     app.redis = redis
 
 
-# 订阅redis日志任务(是否废弃？)
-# FIXME
-# @app.listener('after_server_start')
-# async def sub_redis_log(app, loop):
-#     channel, = await app.redis.subscribe('ops_log')
-#     app.add_task(log_sub(channel))
-
-
-# 关闭redis连接池
 @app.listener('before_server_stop')
 async def close_redis_pool(app, loop):
     app.redis.close()
     await app.redis.wait_closed()
 
 
-# 捕捉404路由
 @app.exception(NotFound)
 async def ignore_404s(request, exception):
     return text(f"Too naive!!! {request.path} is not allowed, baby!")
 
 if __name__ == "__main__":
-    app.run(host=app.config.SERVER_HOST,
-            port=app.config.SERVER_PORT,
-            debug=app.config.SERVER_DEBUG
-            )
+    app.run(**server_settings)
