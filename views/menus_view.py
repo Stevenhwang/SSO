e1cbc01f14a5e1e174eded1939bd3789a663c58a
zed <zed@itcom888.com>
2020-01-15 16:37:24 +0800

    menu validate

--- views/menus_view.py
+++ views/menus_view.py
@@ -24,10 +24,16 @@ class MenusView(HTTPMethodView):
 class MenuView(HTTPMethodView):
     async def put(self, request, mid):
         data = request.json
+        if not data.get('name'):
+            return json(dict(code=-1, msg='关键参数不能为空'))
         m = await Menu.get_or_none(id=mid)
         if not m:
             return json(dict(code=-1, msg='菜单不存在'))
         for k, v in data.items():
+            if k == 'name':
+                em = await Menu.get_or_none(name=v)
+                if em and m.name != v:
+                    return json(dict(code=-1, msg='此菜单已注册！'))
             setattr(m, k, v)
         await m.save()
         return json(dict(code=0, msg='更新成功'))
@@ -38,18 +44,3 @@ class MenuView(HTTPMethodView):
             return json(dict(code=-1, msg='菜单不存在'))
         await m.delete()
         return json(dict(code=0, msg='删除成功'))
-
-    # async def patch(self, request, mid):
-    #     # 菜单启用禁用
-    #     m = await Menu.get_or_none(id=mid)
-    #     if not m:
-    #         return json(dict(code=-1, msg='菜单不存在'))
-    #     status = m.status
-    #     if status:
-    #         m.status = False
-    #         await m.save()
-    #         return json(dict(code=0, msg='菜单禁用成功'))
-    #     else:
-    #         m.status = True
-    #         await m.save()
-    #         return json(dict(code=0, msg='菜单启用成功'))
