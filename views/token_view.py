ef9f654377248a7cc14687288d2eb1a5e7104c13
zed <zed@itcom888.com>
2020-01-20 16:34:01 +0800

    get token

--- views/token_view.py
+++ views/token_view.py
@@ -7,15 +7,15 @@ class TokenView(HTTPMethodView):
         # 查看用户token过期时间
         args = request.ctx.query
         uid = args.get('uid')
-        u = await request.app.redis.get(f"uid_{uid}_auth_token")
-        if not u:
+        token = await request.app.redis.get(f"uid_{uid}_auth_token")
+        if not token:
             return json(dict(code=0, msg='用户未登录!'))
         else:
             ttl = await request.app.redis.execute('ttl', f"uid_{uid}_auth_token")
             if ttl > 0:
-                return json(dict(code=0, msg=f'Token过期时间为{ttl}秒!'))
+                return json(dict(code=0, msg=f'Token: {token.decode()}, TTL: {ttl}秒!'))
             else:
-                return json(dict(code=0, msg=f'长期Token!'))
+                return json(dict(code=0, msg=f'Token: {token.decode()}, TTL: 长期Token!'))
 
     async def post(self, request):
         # 清除token
